8ad199daa12df6781f6df641355a8e194671cabb
guna pranith <pranithreddyguna01@gmail.com>
2022-10-03 15:32:41 +0530

    completed the mappings

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,10 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
-
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
     private String address;
     private String city;
